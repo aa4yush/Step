01d3081569f0c789be9683c8cc94504edee0609e
aa4yush <ayushsithesh@gmail.com>
2026-09-12 11:10:49 +0530

    Add Week 4 homework

---
+++ Week 4/Homework/ProductArray.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class ProductArray {
+    int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+        int left = 1;
+        for (int i = 0; i < n; i++) {
+            answer[i] = left;
+            left = left * nums[i];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] = answer[i] * right;
+            right = right * nums[i];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        ProductArray obj = new ProductArray();
+        int[] result = obj.productExceptSelf(nums);
+        for (int x : result) {
+            System.out.print(x + " ");
+        }
+    }
+}
